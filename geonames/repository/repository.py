c6143afd4207c9147fc0b1438dd9f557b97a9ce3
Lipatov Alexander <tellmeac@gmail.com>
2022-03-30 20:00:49 +0700

    fix: docs

--- geonames/repository/repository.py
+++ geonames/repository/repository.py
@@ -1,13 +1,33 @@
-from typing import Protocol
+from typing import Protocol, List
+
+from geonames.models.geodata import GeoData
 
 
 class GeodataRepository(Protocol):
-
-    def get_rows(self, limit: int, offset: int) -> list:
+    """
+    Base GeoData repository interface
+    """
+    def get_rows(self, limit: int, offset: int) -> List[GeoData]:
+        """
+        Gets rows with offset and limit
+        :param limit:
+        :param offset:
+        :return: list of geodata models
+        """
         pass
 
-    def search_by_name(self, name: str):
+    def search_by_name(self, name: str) -> List[GeoData]:
+        """
+        Searches GeoData by its name
+        :param name:
+        :return: list of geodata models
+        """
         pass
 
-    def search_by_name_part(self, name_part: str):
+    def search_by_name_part(self, name_part: str) -> List[GeoData]:
+        """
+        Searches GeoData by its name part
+        :param name_part:
+        :return: list of geodata models
+        """
         pass
